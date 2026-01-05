e04f5abd20ebb1fd097bd830c66825080460ac1d
khamir <khamir@fp-ws-b160.psycho.unibas.ch>
2026-01-05 10:26:58 +0100

    fixed-threshold spherical diffusion model

--- CRDDM/Models/SDM.py
+++ CRDDM/Models/SDM.py
@@ -40,3 +40,64 @@ def simulate_SDM_trial(threshold, drift_vec, ndt, s_v=0, s_a=0, s_t=0, sigma=1, 
     theta2 = np.arctan2(x[2], x[1])
 
     return ndt_t+rt, (theta1, theta2)
+
+
+def rng(threshold, drift_vec, ndt, s_v=0, s_a=0, s_t=0, sigma=1, dt=0.001, n_sample=1):    
+    RT = np.empty((n_sample,))
+    Choice = np.empty((n_sample, 2))
+
+    for n in range(n_sample):
+        RT[n], Choice[n, :] = simulate_SDM_trial(threshold, drift_vec.astype(np.float64), ndt, 
+                                                 s_v=s_v, s_a=s_a, s_t=s_t, sigma=sigma, dt=dt)
+    
+    return pd.DataFrame(np.c_[RT, Choice], columns=['rt', 'response1', 'response2'])
+
+# The firs-passage time distribution of zero-drift process for small RTs
+@jit(nopython=True)
+def short_t_fpt_z(t, x):
+    term1 = ((1 - x)*(1 + t)**2.5) / ((x + t) * t**1.5)
+    term2 = np.exp(-.5*(1-x)**2/t -  0.5*np.pi**2*t)
+    return term1*term2
+
+# The firs-passage time distribution of zero-drift process
+@jit(nopython=True)
+def long_t_fpt_z(t, threshold, sigma=1, max_n=500):
+    fpt_z = np.zeros(t.shape)
+    for n in range(1, max_n):
+        s = (-1)**(n+1) * n**2 * np.exp(- (n**2 * np.pi**2 * t)/(2*threshold**2))
+        fpt_z += np.pi**2/threshold**2 * s
+    return fpt_z
+
+
+@jit(nopython=True)
+def joint_lpdf(rt, theta, threshold, drift_vec, ndt, s_v=0, s_a=0, s_t=0, sigma=1):
+    tt = np.maximum(rt - ndt, 0)
+    s = tt/threshold**2
+    
+    s0 = 0.002
+    s1 = 0.02
+    w = np.minimum(np.maximum((s - s0) / (s1 - s0), 0), 1)
+    
+    # first-passage time density of zero drift process
+    fpt_lt = long_t_fpt_z(tt, threshold, sigma=sigma)
+    fpt_st = 1/threshold**2 * short_t_fpt_z(tt/threshold**2, 0.1**8/threshold**2)   
+    fpt_z =  (1 - w) * fpt_st + w * fpt_lt
+    fpt_z = np.maximum(fpt_z, 0.1**14)
+
+
+    # Girsanov: no drift variability
+    if s_v == 0:
+        mu_dot_x0 = drift_vec[0]*np.cos(theta[:, 0])
+        mu_dot_x1 = drift_vec[1]*np.sin(theta[:, 0])*np.cos(theta[:, 1]) 
+        mu_dot_x2 = drift_vec[2]*np.sin(theta[:, 0])*np.sin(theta[:, 1])
+        term1 = threshold * (mu_dot_x0 + mu_dot_x1 + mu_dot_x2)
+        term2 = 0.5 * np.linalg.norm(drift_vec, 2)**2 * tt
+        
+        log_density = term1 - term2 + np.log(fpt_z) - np.log(2*np.pi)
+    else:
+        pass # to be implemented later
+
+    log_density[rt - ndt <= 0] = np.log(0.1**14)
+    log_density = np.maximum(log_density, np.log(0.1**14))
+        
+    return log_density
