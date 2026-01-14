24071aec1375a8e3ac4a13a7c1b2f227fdb9a99f
AmirHoseinHadian <amir.h.hadian@gmail.com>
2026-01-14 11:58:34 +0100

    collapsing threshold HSDM added

--- CRDDM/Models/HSDM.py
+++ CRDDM/Models/HSDM.py
@@ -56,3 +56,44 @@ class fixedThresholdHSDM:
             
         return log_density
     
+
+class collapsingThresholdHSDM:
+    def __init__(self):
+        self.name = 'Hyper-Spherical Diffusion Model with collapsing boundaries'
+
+    def simulate(self, threshold, decay, drift_vec, ndt, s_v=0, s_t=0, sigma=1, dt=0.001, n_sample=1):
+        RT = np.empty((n_sample,))
+        Choice = np.empty((n_sample, 3))
+
+        for n in range(n_sample):
+            RT[n], Choice[n, :] = simulate_HSDM_trial(threshold, drift_vec.astype(np.float64), ndt, 
+                                                      decay=decay, s_v=s_v, s_t=s_t, sigma=sigma, dt=dt)
+        
+        return pd.DataFrame(np.c_[RT, Choice], columns=['rt', 'response1', 'response2', 'response3'])
+    
+    def joint_lpdf(self, rt, theta, threshold, decay, drift_vec, ndt, s_v=0, s_t=0, sigma=1):
+        tt = np.maximum(rt - ndt, 0)
+
+        T_max = min(rt.max(), threshold/decay)
+        g_z, T = ie_fpt(threshold, decay, 4, 0.000001, dt=0.02, T_max=T_max)
+        
+        fpt_z = np.interp(tt, T, g_z)
+        fpt_z = np.maximum(fpt_z, 0.1**14)
+
+        # Girsanov: no drift variability
+        if s_v == 0:
+            mu_dot_x0 = drift_vec[0]*np.cos(theta[:, 0])
+            mu_dot_x1 = drift_vec[1]*np.sin(theta[:, 0])*np.cos(theta[:, 1]) 
+            mu_dot_x2 = drift_vec[2]*np.sin(theta[:, 0])*np.sin(theta[:, 1])*np.cos(theta[:, 2])
+            mu_dot_x3 = drift_vec[3]*np.sin(theta[:, 0])*np.sin(theta[:, 1])*np.sin(theta[:, 2])
+            term1 = (threshold - decay*tt) * (mu_dot_x0 + mu_dot_x1 + mu_dot_x2 + mu_dot_x3)
+            term2 = 0.5 * np.linalg.norm(drift_vec, 2)**2 * tt
+
+            log_density = term1 - term2 + np.log(fpt_z) - np.log(2*np.pi)
+        else:
+            pass # to be implemented later
+
+        log_density[rt - ndt <= 0] = np.log(0.1**14)
+        log_density = np.maximum(log_density, np.log(0.1**14))
+            
+        return log_density
