4d289f276da1a0c8bbe68be1b294d8f5a2711658
AmirHoseinHadian <amir.h.hadian@gmail.com>
2026-01-26 13:39:19 +0100

    kvam dataset loader

--- CRDDM/utility/datasets.py
+++ CRDDM/utility/datasets.py
@@ -1,4 +1,5 @@
 import os
+import numpy as np
 import pandas as pd
 
 __dir__ = os.path.abspath(os.path.dirname(__file__))
@@ -16,4 +17,27 @@ def load_fennell2023():
     data_path = os.path.join(os.path.dirname(os.path.dirname(__dir__)), "data", "Fennell2023_exp1.csv")
 
     data = pd.read_csv(data_path, index_col=0)
+    return data
+
+
+def load_kvam2019():
+    '''
+    Load data from Kvam et al. (2019).
+
+    Returns
+    -------
+    pd.DataFrame
+        A dataframe containing the data with columns: 
+    '''
+
+    data_path = os.path.join(os.path.dirname(os.path.dirname(__dir__)), "data", "Kvam2019.csv")
+
+    data = pd.read_csv(data_path)
+
+    data = data.drop(columns=['isCued', 'cueOrientation', 
+                                'cueDeflections', 'points', 
+                                'jitterLevel', 'targetOrientation'])
+    data = data.rename(columns={'RT':'rt'})
+    data.deviation *= 2
+    data.absoluteDeviation = np.abs(data.deviation)
     return data
