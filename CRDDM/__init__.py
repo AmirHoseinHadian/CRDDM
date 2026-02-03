4483d6305f9a43771902e611c25d7220563a2f37
AmirHoseinHadian <amir.h.hadian@gmail.com>
2026-02-03 15:00:41 +0100

    installation issue on colab

--- CRDDM/__init__.py
+++ CRDDM/__init__.py
@@ -1,11 +1,16 @@
 from __future__ import absolute_import, division, print_function
 from .version import __version__  # 
-from CRDDM.Models.Circular import *
-from CRDDM.Models.Spherical import *
-from CRDDM.Models.HyperSpherical import *
+from .Models.Circular import *
+from .Models.Spherical import *
+from .Models.HyperSpherical import *
 
-from CRDDM.utility.Constants import *
-from CRDDM.utility.datasets import *
-from CRDDM.utility.fpts import *
-from CRDDM.utility.helpers import *
-from CRDDM.utility.simulators import *
+from .utility.Constants import *
+from .utility.datasets import *
+from .utility.fpts import *
+from .utility.helpers import *
+from .utility.simulators import *
+
+__all__ = [
+    "Models",
+    "utility",
+]
